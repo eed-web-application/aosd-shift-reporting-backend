082582dca418020d6496c6cd1421b7050c74cc6c
poonamslac <poonam@slac.stanford.edu>
2024-03-05 12:47:47 -0800

    Database user change
    Database user changed after postgres db instance crash.

--- src/pg_connection.js
+++ src/pg_connection.js
@@ -1,6 +1,6 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
+  user: 'reliability',
   host: '172.24.5.194',
   database: 'reliability',
   password: 'L4gkpRq03JSCn4FxukuiTeKT9q2ogYTOJh0BJVGXfDRWTt8ehKDyhw89RKRyLAFR',
